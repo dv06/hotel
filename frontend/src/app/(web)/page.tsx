336ebe1e5d9ea6205916730262b26448c61f3356
Devanshi <devanshi.mishra.06@gmail.com>
2023-12-22 16:28:13 -0500

    Added Search functionality and static gallery and newsletter

--- frontend/src/app/(web)/page.tsx
+++ frontend/src/app/(web)/page.tsx
@@ -1,6 +1,16 @@
+import Gallery from '@/components/Gallery/Gallery';
 import HeroSection from '@/components/HeroSection/HeroSection';
+import NewsLetter from '@/components/Newsletter/Newsletter';
+import PageSearch from '@/components/PageSearch/PageSearch';
 
 const Home = () => {
-  return <HeroSection />;
+  return (
+    <>
+      <HeroSection />
+      <PageSearch />
+      <Gallery />
+      <NewsLetter />
+    </>
+  );
 };
 export default Home;
